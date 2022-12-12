9ac89bcfd6d36eee3cb7a8d884a62f971399945e
RAguirreGiraldo <raguirregiraldo@gmail.com>
2022-12-12 11:29:56 -0600

    Update javaScript Token mapbox

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,2 +1,11 @@
 'use strict';
 
+const accessToken = 'pk.eyJ1Ijoicm9iZXJ0b2FndWlycmUxOTc1IiwiYSI6ImNsYmwyamE0cTAzZzIzdW13OG9uc2tnZDgifQ.weAKBXx9xj0TFLKklrdZyg';
+
+const script = document.getElementById('search-js');
+
+script.onload = function() {
+    mapboxsearch.autofill({
+    accessToken
+    });
+};
